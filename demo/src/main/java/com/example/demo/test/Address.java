001bb17869f5efa47eb4406ae912b86b3352c70f
Mishra <AkaMishra@ad.harman.com>
2020-07-06 15:21:30 +0530

    Demo pushed

---
+++ demo/src/main/java/com/example/demo/test/Address.java
@@ -0,0 +1,28 @@
+package com.example.demo.test;
+
+public class Address {
+	
+	String streetAddress;
+	int pinCode;	
+	
+	public Address(String streetAddress, int pinCode) {
+		super();
+		this.streetAddress = streetAddress;
+		this.pinCode = pinCode;
+	}	
+	
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	public int getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(int pinCode) {
+		this.pinCode = pinCode;
+	}
+	
+	
+}
---
+++ demo/src/main/java/com/example/demo/test/Test.java
@@ -0,0 +1,10 @@
+package com.example.demo.test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Success");
+		String str = "";
+		System.out.println("str : " + str.toUpperCase());
+	}
+}
